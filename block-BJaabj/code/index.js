28fb0f0447d38812cfbdc23aba8b7847da5839ca
bhabanisankar1901@gmail.com <bhabanisankar1901@gmail.com>
2023-07-18 09:58:00 +0530

    function assignment complete

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -1,6 +1,9 @@
 /*
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
+function sayhello() {
+  alert(`Hello world`);
+}
 
 /*
 2. Create a function named `fullName` with the following steps:
@@ -10,6 +13,12 @@
   - Concat the value of fistName and lastName and store it in another variable named `fullName`.
   - Alert `fullName`
 */
+function fullName(){
+  let firstName = prompt(`Enter Your first Name`);
+  let lastName = prompt(`Enter Your Last Name`);
+  let full = `$(firstName) $(lastName)`;
+  alert(full);
+}
 
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
@@ -19,6 +28,12 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+function fullName(){
+  let firstNum = +prompt(`Enter first number`);
+  let secondNum = +prompt(`Enter second number`);
+  let sum = firstNum + secondNum;
+  alert(sum);
+}
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -38,6 +53,12 @@
   9 * 9 = 81
   9 * 10 = 90
 */
+function getTable(){
+  let num = +prompt(`Enter a number`);
+  for(let i = 1; i <=10; i++){
+    console.log(`${num} * ${i} = ${num * i}`);
+  }
+}
 
 /*
 5. Create a function named `isLeapYear` with the following steps:
@@ -45,6 +66,14 @@
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+function isLeapYear(){
+  let year = +prompt( `enter a year `);
+  if( year % 4 ===0 ){
+    alert( `${ year}  is leap year`);
+  }else{
+    alert( `${ year} 
+    is not leap year`);
+  }
 
 /*
 6. Create a function named `getFactorial` with the following steps:
@@ -53,3 +82,8 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+fuction getFactoraial(){
+  let number = +prompt("enter a number ");
+  alert(`The factorial is ${number } is ${number + '!'}`);
+}
+}
